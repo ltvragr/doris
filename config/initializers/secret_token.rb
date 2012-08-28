258c4aadcec8eb9d4a15ee08753afe395c1afa8b
Adam Bray <adam.bray@yale.edu>
2012-08-28 14:47:19 -0400

    initial rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doris::Application.config.secret_token = '6c4808ff438c5cf9dba32f5ea46fce6b6feac1c03ea40fd83ab78cf97a432d1b748cf93d5a3e0c6fdd3ba05dac6bfc23203626203c0cfd44227721077c7d1d0a'
