e6eeba54cade284bd537b77ff07d99afcb6fbe48
Danqing <danqing.coding@gmail.com>
2012-09-28 14:36:27 -0400

    I'm just trying to pull

--- app/models/info_field.rb
+++ app/models/info_field.rb
@@ -1,3 +1,20 @@
 class InfoField < ActiveRecord::Base
   attr_accessible :associated_object, :associated_role, :category, :content, :content_type, :label, :sort_order
+
+  def prepare_form_helpers
+    if display_type == "text"
+      return ["data_fields", id, {:id => id}]
+    elsif display_type == "paragraph_text"
+      return ["data_fields", id]
+    elsif display_type == "select_box"
+      options = values.split(',').each{|opt| opt.squish!}
+      return ["data_fields", id, options.map{|opt| [opt, opt]}]
+    elsif display_type == "check_box"
+      options = values.split(',').each{|opt| opt.squish!}
+      return options.map{|v| ["data_fields[#{id}]", v]}
+    elsif display_type == "radio_button"
+      options = values.split(',').each{|opt| opt.squish!}
+      return options.map{|v| ["data_fields[#{id}]", 1, v]}
+    end
+  end
 end
