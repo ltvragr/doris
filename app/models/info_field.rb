2896787bb256cb5ff3eae1aee3190767d8711e63
Adam Bray <adam.bray@yale.edu>
2012-09-28 14:34:32 -0400

    appropriate fields are hard coded for info fields, associated_object, etc

--- app/models/info_field.rb
+++ app/models/info_field.rb
@@ -1,3 +1,15 @@
 class InfoField < ActiveRecord::Base
+  DISPLAY_TYPE_OPTIONS = { "Text Field"   => "text_field",
+                           "Paragraph Text"    => "text_area",
+                           "Select from a List"   => "select",
+                           "Multiple Choice" => "radio_button",
+                           "Check Boxes" => "check_box"}
+  ASSOCIATED_OBJECT_OPTIONS = { "User" => "user",
+  							    "Lab" => "lab"}
+
+  ASSOCIATED_ROLE_OPTIONS = { 	"Undergrad"   => "undergrad",
+                   	"PI"    => "pi",
+					"Graduate Student"   => "graduate"}
+
   attr_accessible :associated_object, :associated_role, :category, :content, :content_type, :label, :sort_order
 end
