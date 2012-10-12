ba3cfa82e58df77eb119e3ec950fd694a030a5e0
Adam Bray <adam.bray@yale.edu>
2012-10-11 23:52:01 -0400

    basic viewing of info_values on user show page

--- app/models/info_value.rb
+++ app/models/info_value.rb
@@ -3,9 +3,9 @@ class InfoValue < ActiveRecord::Base
 
   attr_accessible :associated_object_id, :associated_object_type, :content
 
-  def sorted_values_for_object(object)
-  	#This needs to be re-written now that object_type is inferred from info_field
-  	#InfoValue.where('associated_object_type' = object.class).where('associated_object_id' = object.id)
+  def self.sorted_values_for_object(object)
+  	#TODO This should  be re-written so that object_type is inferred from info_field
+  	InfoValue.where('associated_object_id' => object.id).select{|iv| iv.info_field.associated_object == object.class.name}
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :login, :status
 
   has_and_belongs_to_many :projects
-  
+
+  def name
+    first_name + " " + last_name
+  end  
 end
