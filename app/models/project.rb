c40874218e42ed5d1b11df74c85bab314f1d93fa
br.araki <br.araki@gmail.com>
2012-11-12 17:14:35 -0500

    added tokenInput and made Projects page display Labs

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,13 @@
 class Project < ActiveRecord::Base
-  attr_accessible :description, :end_date, :name, :start_date
+  attr_accessible :description, :end_date, :name, :start_date, :lab_tokens
 
   has_and_belongs_to_many :users
   has_and_belongs_to_many :labs
-  
+
+  attr_reader :lab_tokens
+
+  def lab_tokens=(ids)
+    self.lab_ids = ids.split(",")
+  end
+
 end
