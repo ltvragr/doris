ea221b222417d9839f50ac0554295d6780eed223
Adam Bray <adam.bray@yale.edu>
2012-12-06 17:56:40 -0500

    initial seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,84 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'ffaker'
+
+#Method for prompting the user for the number of records per model they want to seed into the database.
+def ask_for_records(model)
+  STDOUT.puts "\nHow many #{model} records would you like to generate? (please enter a number)"
+  STDIN.gets.chomp.to_i
+end
+
+#Random Time
+def time_rand(from = 0.0, to = Time.now, length = 0)
+  range = to.to_f - from.to_f
+
+  if range > length.to_f
+    range = length.to_f
+  end
+
+  Time.at(from + rand * range)
+end
+
+#Random object that is used throughout for generating fake data that Faker can't
+r = Random.new
+
+####################
+#Seed Admin user
+####################
+if User.all.empty?  
+  USER_ATTRS = [:first_name, :last_name, :email, :login, :status]
+  user_data = {}
+  STDOUT.puts "We need to create an account for you first."  
+    USER_ATTRS.each do |a|
+      STDOUT.puts a.to_s  
+      user_data[a] = STDIN.gets.chomp
+    end
+  
+  user = User.create!(user_data)
+  user.add_role "admin"
+  user.add_role "undergrad"
+  user.save!
+end
+
+
+####################
+#Seed other users
+####################
+entered_num = ask_for_records("User")
+
+if entered_num.integer? && entered_num > 0
+  users = entered_num.times.map do
+    User.create do |u|
+      u.first_name =Faker::Name.first_name
+      u.last_name =Faker::Name.last_name
+      u.email =Faker::Internet.email
+      u.login = (0...3).map{65.+(rand(25)).chr}.join.downcase + r.rand(2..99).to_s
+      u.status = "undergrad"
+    end
+  end
+  STDOUT.puts "\n#{entered_num} records successfully created!"
+  users << User.first
+else
+  STDOUT.puts "\nPlease enter a whole number greater than 0."
+  entered_num = STDIN.gets.chomp.to_i
+end
+
+####################
+#Seed Labs
+####################
+entered_num = ask_for_records("Labs")
+
+if entered_num.integer? && entered_num > 0
+  labs = entered_num.times.map do
+    Lab.create do |l|
+      l.name =Faker::Name.last_name + "'s Lab"
+      l.description =Faker::Company.catch_phrase
+      l.url = Faker::Internet.email
+    end
+  end
+  STDOUT.puts "\n#{entered_num} records successfully created!"
+else
+  STDOUT.puts "\nPlease enter a whole number greater than 0."
+  entered_num = STDIN.gets.chomp.to_i
+end
