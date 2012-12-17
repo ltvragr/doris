7f9914aca41229452c222d52dc074f668c882463
Danqing <danqing.coding@gmail.com>
2012-12-17 17:44:14 -0500

    Droped role from user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121129013821) do
+ActiveRecord::Schema.define(:version => 20121217224127) do
 
   create_table "info_fields", :force => true do |t|
     t.string   "associated_object"
@@ -86,7 +86,6 @@ ActiveRecord::Schema.define(:version => 20121129013821) do
     t.string   "status"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "role"
   end
 
   create_table "users_roles", :id => false, :force => true do |t|
