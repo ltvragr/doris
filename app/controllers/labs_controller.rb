eeaafd541bb6e148726c5273c9147528946dbe41
br.araki <br.araki@gmail.com>
2013-03-08 20:38:04 -0500

    finally made it so that you cant add a lab for a PI who already has a lab

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -46,28 +46,31 @@ class LabsController < ApplicationController
     # Fill in missing fields for a requested lab
     if current_user.status == "undergrad"
       @lab.is_authorized = false
-      #pi_params = User.ldap_search(params[:lab][:user_tokens])[0]
-      #pi = User.new(login: pi_params[:login], first_name: pi_params[:first_name], last_name: pi_params[:last_name], email: pi_params[:email])
       @lab.name = @lab.principles.first.last_name + " Lab"
-      #pi.save
       @lab.url = ""
       @lab.description = "This lab is unauthorized."
       #send an email to PI
+      pi = User.joins(:labs).where(:id => @lab.principles.first.id)
     else
       @lab.is_authorized = true
     end
     
     respond_to do |format|
-      if @lab.save
-        if current_user.status == "undergrad"
-          format.html { redirect_to labs_url, notice: 'Lab was successfully created. Your PI will be notified shortly.' }
+      if pi.size == 0
+        if @lab.save
+          if current_user.status == "undergrad"
+            format.html { redirect_to labs_url, notice: 'Lab was successfully created. Your PI will be notified shortly.' }
+          else
+            format.html { redirect_to @lab, notice: 'Lab was successfully created.' }
+          end
+          format.json { render json: @lab, status: :created, location: @lab }
         else
-          format.html { redirect_to @lab, notice: 'Lab was successfully created.' }
+          format.html { render action: "new" }
+          format.json { render json: @lab.errors, status: :unprocessable_entity }
         end
-        format.json { render json: @lab, status: :created, location: @lab }
       else
-        format.html { render action: "new" }
-        format.json { render json: @lab.errors, status: :unprocessable_entity }
+        format.html { redirect_to labs_url, notice: 'This PI already has a lab' }
+        format.json { render json: @lab}
       end
     end
   end
